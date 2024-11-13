0c726f551829e34ab18f125d664a5c9faf9b2688
wesleywmg2609 <wesleywongminguan@gmail.com>
2024-11-13 22:24:20 +0800

    Modified 2 files.

--- lib/views/2_page.dart
+++ lib/views/2_page.dart
@@ -1,5 +1,9 @@
+import 'package:easy_date_timeline/easy_date_timeline.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:tanku/components/my_box_shadow.dart';
+import 'package:tanku/components/my_button.dart';
+import 'package:tanku/components/my_text.dart';
 
 // ignore: must_be_immutable
 class Page2 extends StatefulWidget {
@@ -15,6 +19,8 @@ class Page2 extends StatefulWidget {
 }
 
 class _Page2State extends State<Page2> {
+  MyBoxShadows shadows = MyBoxShadows();
+  DateTime _focusDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
@@ -23,7 +29,82 @@ class _Page2State extends State<Page2> {
         body: SafeArea(
             child: Column(
           children: [
-            Expanded(child: Center()),
+            EasyDateTimeLine(
+              initialDate: _focusDate,
+              onDateChange: (selectedDate) {
+                setState(() {
+                  _focusDate = selectedDate;
+                });
+              },
+              headerProps: EasyHeaderProps(
+                  monthPickerType: MonthPickerType.switcher,
+                  dateFormatter: const DateFormatter.fullDateMonthAsStrDY(),
+                  selectedDateStyle: TextStyle(
+                    fontFamily: 'SFPro',
+                    color: Theme.of(context).colorScheme.onSurface,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.0,
+                    fontSize: 14,
+                  ),
+                  monthStyle: TextStyle(
+                    fontFamily: 'SFPro',
+                    color: Theme.of(context).colorScheme.onSurface,
+                    letterSpacing: 2.0,
+                    fontSize: 14,
+                  ),
+                  padding: const EdgeInsets.only(left: 16)
+                ),
+              timeLineProps:
+                  const EasyTimeLineProps(
+                    separatorPadding: 10
+                  ),
+              dayProps: const EasyDayProps(
+                height: 128,
+              ),
+              itemBuilder: (
+                BuildContext context,
+                DateTime date,
+                bool isSelected,
+                VoidCallback onTap,
+              ) {
+                return SizedBox(
+                  width: 96,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0),
+                    child: MyButton(
+                      onPressed: onTap,
+                      resetAfterPress: false,
+                      isPressed: !isSelected,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          MyText(
+                            text: EasyDateFormatter.shortMonthName(date, "en_US")
+                                .toUpperCase(),
+                            letterSpacing: 2.0,
+                            size: 14,
+                          ),
+                          const SizedBox(height: 5),
+                          MyText(
+                            text: date.day.toString(),
+                            letterSpacing: 2.0,
+                            isBold: true,
+                            size: 20,
+                          ),
+                          const SizedBox(height: 5),
+                          MyText(
+                            text: EasyDateFormatter.shortDayName(date, "en_US")
+                                .toUpperCase(),
+                            letterSpacing: 2.0,
+                            size: 14,
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
           ],
         )));
   }
